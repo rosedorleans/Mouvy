7d037d3286384fe7d8bff9f1d27c75a8a0d67c7e
rose.dorleans <rose.dorleans@autobonplan.com>
2022-01-24 17:09:32 +0100

    feat search

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,39 @@
-const url_base = "https://apis.justwatch.com/content/";
-const local = "/fr_FR";
-const content_type = null;
-const providers = null;
-const genres = null;
+const url_base = "https://www.omdbapi.com/?apikey=72357c68&";
 
-function find_providers(){
-    $.ajax ({
-        url: url_base + 'providers/locale' + local,
+$("#movie_search_btn").off('click');
+$('#movie_search_btn').on('click', function() {
+    let val = document.querySelector('#movie_search').value;
+    console.log(val);
+    $.ajax({
+        url: url_base + 't=' + val,
         type: 'GET',
-        success : function(reponse){
-            console.log(reponse);
+        success: function (response) {
+            console.log(response)
+            $('.popup').removeClass('hidden');
+            $('.popup-overlay').removeClass('hidden');
+            $("html").css("overflow", 'hidden');
+            $("#movie_page").append('' +
+                '<p>'+response.Title+'</p>' +
+                '<p></p>')
         }
+    });
+    $('.close').click(function(){
+        $("html").css("overflow", 'scroll');
+        $('.popup-overlay').addClass('hidden');
+        $('.popup').addClass('hidden');
+        $('#movie_page').empty();
     })
-}
+});
 
-function find_genres(){
-    $.ajax ({
-        url: url_base + 'genres/locale' + local,
-        type: 'GET',
-        success : function(reponse){
-            console.log(reponse);
-        }
-    })
-}
 
-find_genres()
-find_providers()
+
+
+
+
+
+
+
+
+
+
+
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,8 +13,31 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
+
+    /**
+     * @Route("/platforms", name="platforms")
+     */
+    public function platforms(): Response
+    {
+        return $this->render('pages/platforms.html.twig');
+    }
+
+    /**
+     * @Route("/movies", name="movies")
+     */
+    public function movies(): Response
+    {
+        return $this->render('pages/movies.html.twig');
+    }
+
+    /**
+     * @Route("/movie", name="movie")
+     */
+    public function movie(): Response
+    {
+        return $this->render('pages/movie.html.twig');
+    }
+
 }
